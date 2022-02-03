755e6abcb6e145de6f0f7e7c81e7b6e0346c157d
Gigiox <guillermo.casanova.b@gmail.com>
2022-02-03 20:51:27 -0300

    Login de usuario con firebase

--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -13,15 +13,35 @@ export const createUser = async ({ commit }, user ) => {
         const { data } = await authApi.post(':signUp', { email, password, returnSecureToken: true })
         const { idToken, refreshToken } = data
 
-        await authApi.post(':update', { displayName: name, idToken })
+        await authApi.post(':update', { displayName: name, idToken, refreshToken })
 
-        console.log(refreshToken, commit)
+        delete user.password
+        commit('loginUser', { user, idToken, refreshToken})        
 
-        // todo Mutation: loginUser
         return { ok: true }
 
     } catch ( error ) {
-        console.log(error.response)
+        return { ok: false, message: error.response.data.error.message }
+    }
+
+}
+
+export const signInUser = async ({ commit }, user ) => {
+
+    const { email, password } = user
+    
+    try {
+
+        const { data } = await authApi.post(':signInWithPassword', { email, password, returnSecureToken: true })
+        const { displayName, idToken, refreshToken } = data
+
+        user.name = displayName
+
+        commit('loginUser', { user, idToken, refreshToken})        
+
+        return { ok: true }
+
+    } catch ( error ) {
         return { ok: false, message: error.response.data.error.message }
     }
 
