612f75fe964fe7a2ad9cc1ac84e67b90fa13b947
Gigiox <guillermo.casanova.b@gmail.com>
2022-02-07 19:02:17 -0300

    testing loginUser success, failed and checkAuthStatus

--- tests/unit/modules/auth/composables/useAuth.spec.js
+++ tests/unit/modules/auth/composables/useAuth.spec.js
@@ -30,4 +30,66 @@ describe('Pruebas en useAuth', () => {
 
     })
 
+    test('createUser - failed, porque el usuario ya existe', async() => {
+
+        const { createUser } = useAuth()
+
+        const newUser = { name: 'Guillermo', email: 'guillermo@gmail.com' }
+
+        mockStore.dispatch.mockReturnValue({ ok: false, message: 'EMAIL_EXISTS' })
+
+        const resp = await createUser( newUser )
+
+        expect( mockStore.dispatch ).toHaveBeenCalledWith('auth/createUser', newUser)
+
+        expect( resp ).toEqual({ ok: false, message: 'EMAIL_EXISTS' })
+
+    })
+
+    test('loginUser - success', async() => {
+
+        const { loginUser } = useAuth()
+
+        const loginForm = { email: 'test@gtest.com', password: '123456' }
+
+        mockStore.dispatch.mockReturnValue({ ok: true })
+
+        const resp = await loginUser( loginForm )
+
+        expect( mockStore.dispatch ).toHaveBeenCalledWith('auth/signInUser', loginForm)
+
+        expect( resp ).toEqual({ ok: true })
+
+    })
+
+    test('loginUser - failed', async() => {
+
+        const { loginUser } = useAuth()
+
+        const loginForm = { email: 'test@gtest.com', password: '123456'}
+
+        mockStore.dispatch.mockReturnValue({ ok: false, message: 'EMAIL/PASSWORD do not exist' })
+
+        const resp = await loginUser( loginForm )
+
+        expect( mockStore.dispatch ).toHaveBeenCalledWith('auth/signInUser', loginForm)
+
+        expect( resp ).toEqual({ ok: false, message: 'EMAIL/PASSWORD do not exist' })
+
+    })
+
+    test('checkAuthStatus', async() => {
+
+        const { checkAuthStatus } = useAuth()
+
+        mockStore.dispatch.mockReturnValue({ ok: true })
+
+        const resp = await checkAuthStatus()
+
+        expect( mockStore.dispatch ).toHaveBeenCalledWith('auth/checkAuthentication')
+
+        expect( resp ).toEqual({ ok: true })
+
+    })
+
 })
