267839908d99db3dc26746446ad6d886c30eae94
Gigiox <guillermo.casanova.b@gmail.com>
2022-02-07 21:26:34 -0300

    Testing credentials and trigger sweetalert error

--- tests/unit/modules/auth/views/login.spec.js
+++ tests/unit/modules/auth/views/login.spec.js
@@ -3,6 +3,13 @@ import Login from '@/modules/auth/views/Login.vue'
 
 import createVuexStore from '../../../mock-data/mock-store'
 
+import Swal from 'sweetalert2'
+
+jest.mock('sweetalert2', () => ({
+    fire: jest.fn(),
+    showLoading: jest.fn(),
+    close: jest.fn()
+}))
 
 describe('Pruebas en el Login Component', () => {
 
@@ -13,6 +20,8 @@ describe('Pruebas en el Login Component', () => {
         refreshToken: null
     })
 
+    store.dispatch = jest.fn()
+
     beforeEach(() => jest.clearAllMocks() )
 
     test('Debe de hacer match con el snapshot', () => {
@@ -21,9 +30,25 @@ describe('Pruebas en el Login Component', () => {
             global: {
                 plugins: [ store ]
             }
-        } )
+        })
 
         expect( wrapper.html() ).toMatchSnapshot()
+
+    })
+
+    test('Credenciales incorrectas, disparar error de SweetAlert', async() => {
+
+        store.dispatch.mockReturnValueOnce({ ok: false, message: 'Credenciales incorrectas'})
+
+        const wrapper = shallowMount( Login, {
+            global: {
+                plugins: [ store ]
+            }
+        })
+
+        await wrapper.find('form').trigger('submit')
+        expect( store.dispatch ).toHaveBeenCalledWith('auth/signInUser', { 'email': '', 'password': '' })
+        expect( Swal.fire ).toHaveBeenCalledWith( 'Error', 'Credenciales incorrectas', 'error' )
 
     })
 
